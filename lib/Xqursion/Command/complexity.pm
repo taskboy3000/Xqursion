9a4aa29907413862923270ef378c56f778a822d6
Joe Johnston <jjohn@taskboy.com>
2015-12-10 09:42:04 -0500

    improved the complexity report

--- lib/Xqursion/Command/complexity.pm
+++ lib/Xqursion/Command/complexity.pm
@@ -5,11 +5,12 @@ use Mojo::Home;
 use File::Find;
 use Perl::Metrics::Simple::Analysis::File;
 use Data::Dumper;
-
+use List::Util ('sum');
 has description => "For each perl module, describe the complexity of each";
 has usage => "";
 
 our @Problems;
+our @Complexities;
 
 sub run {
     my ($self) = shift;
@@ -17,9 +18,13 @@ sub run {
     $home->detect;
 
     find(\&_report_complexity, $home->to_string . "/../lib");
-    
+
+    my $avg = (sum @Complexities)/scalar @Complexities;
+            printf "%s\n", ("="x60);
+
+    printf("AVERAGE COMPLEXITY: %0.2f\n\n", $avg);
+
     if (@Problems) {
-        printf "%s\n", ("="x60);
         printf ("The following subroutines are particularly complex\n");
         for my $p (@Problems) {
             printf "%55s: ", _trim($p->{file}) . "[" . $p->{sub} . "]";
@@ -33,13 +38,14 @@ sub _report_complexity {
 
     my $analyzer = Perl::Metrics::Simple::Analysis::File->new(path => $File::Find::name);
     my $stats = $analyzer->all_counts;
-    printf("File: %s\n", _trim($File::Find::name));
+    printf("\nFile: %s\n", _trim($File::Find::name));
 
     for my $key ('main_stats') {
         printf("  %-35s: lines: %3d; complexity: %d\n", 
                "main", 
                $stats->{$key}->{lines}, 
                $stats->{$key}->{mccabe_complexity});
+               push @Complexities, $stats->{mccabe_complexity} if $stats->{mccabe_complexity};
     }
 
     for my $sub (@{$stats->{subs}}) {
@@ -47,6 +53,7 @@ sub _report_complexity {
                $sub->{name}, 
                $sub->{lines}, 
                _markup($sub->{mccabe_complexity}));
+        push @Complexities, $sub->{mccabe_complexity} if $sub->{mccabe_complexity};
 
         if ($sub->{mccabe_complexity} > 10) {
             push @Problems, { file => $File::Find::name, 
@@ -54,6 +61,7 @@ sub _report_complexity {
                               lines => $sub->{lines},
                               complexity => $sub->{mccabe_complexity}
                             };
+
         }
     }
     print "-------\n";
